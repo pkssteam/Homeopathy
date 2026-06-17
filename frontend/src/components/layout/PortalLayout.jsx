9f7181b9303c4bb44c01f4ad04a4a0f6e9382193
Gayathiri <sureshgpkss@gmail.com>
2026-06-17 13:24:04 +0530

    Footer

--- frontend/src/components/layout/PortalLayout.jsx
+++ frontend/src/components/layout/PortalLayout.jsx
@@ -116,8 +116,8 @@ export const PortalLayout = ({ children, activeTab, setActiveTab }) => {
             <div className="portal-max-width">
               {children}
             </div>
-            <Footer />
           </main>
+          <Footer />
         </div>
       </div>
 
